5b10a8742153c1eaa218f38042744c6ba0fc8a82
Kumar Akshat <97528193+Kumar-Akshat22@users.noreply.github.com>
2024-06-22 12:16:44 +0530

    Solved using more optimized approach

--- Arrays-Assignment/FindPivotIndex.cpp
+++ Arrays-Assignment/FindPivotIndex.cpp
@@ -4,14 +4,15 @@
 using namespace std;
 
 // Brute Force Approach
-int findPivot(vector<int>& nums){
+int findPivot(vector<int> &nums)
+{
 
-    int n  = nums.size();
+    int n = nums.size();
     int ans = -1;
 
     for (int i = 0; i < n; i++)
     {
-        int lsum = 0 , rsum = 0;
+        int lsum = 0, rsum = 0;
         // Calculate the left sum
         for (int j = 0; j < i; j++)
         {
@@ -19,20 +20,19 @@ int findPivot(vector<int>& nums){
         }
 
         // Calculate the right sum
-        for (int k = i+1; k < n; k++)
+        for (int k = i + 1; k < n; k++)
         {
             rsum = rsum + nums[k];
         }
-        
+
         // Check whether the lsum and rsum are equal or not
         if (lsum == rsum)
         {
             ans = i;
             break;
         }
-        
     }
-    
+
     return ans;
 
     // T.C -> O(n*n)
@@ -41,25 +41,26 @@ int findPivot(vector<int>& nums){
 
 // Optimized Approach
 // Using Prefix Sum
-int findPivotUsingPrefixSum(vector<int>& nums){
+int findPivotUsingPrefixSum(vector<int> &nums)
+{
 
     int n = nums.size();
     int ans = -1;
 
-    vector<int> lsum(n , 0);
-    vector<int> rsum(n , 0);
+    vector<int> lsum(n, 0);
+    vector<int> rsum(n, 0);
 
-    // Calculate the left sum 
+    // Calculate the left sum
     for (int i = 1; i < n; i++)
     {
-        lsum[i] = lsum[i-1] + nums[i-1];
+        lsum[i] = lsum[i - 1] + nums[i - 1];
     }
 
     // Calculate the right sum
-    for (int i = n-2; i >=0 ; i--)
+    for (int i = n - 2; i >= 0; i--)
     {
 
-        rsum[i] = rsum[i+1] + nums[i+1];
+        rsum[i] = rsum[i + 1] + nums[i + 1];
     }
 
     // Iterate the vector once again to check lsum equals rsum
@@ -70,21 +71,57 @@ int findPivotUsingPrefixSum(vector<int>& nums){
             ans = i;
             break;
         }
-        
     }
     return ans;
-    
+
     // T.C -> O(n)
     // S.C -> O(n)
-
-
 }
 
-int main(){
+// More Optimized in terms of time and space complexity
+int findPivotIndexOptimized(vector<int> &nums)
+{
 
-    vector<int> nums = {1,7,3,6,5,6};
+    int n = nums.size();
+    int lsum = 0;
+    int rsum = 0;
+    int sum = 0;
+    int ans = -1;
 
-    int ans = findPivotUsingPrefixSum(nums);
+    // Calculate the total sum of the vector
+    for (int i = 0; i < n; i++)
+    {
+
+        sum += nums[i];
+    }
+
+    // Iterate the array and check lsum equals rsum for each element
+    for (int i = 0; i < n; i++)
+    {
+
+        rsum = sum - lsum - nums[i];
+        if (lsum == rsum)
+        {
+
+            ans = i;
+            break;
+        }
+
+        lsum = lsum + nums[i];
+    }
+
+    return ans;
+
+    // T.C -> O(n)
+    // S.C -> O(1)
+}
+
+int main()
+{
+
+    vector<int> nums = {1, 7, 3, 6, 5, 6};
+
+    int ans = findPivotIndexOptimized(nums);
     cout << "The pivot index is: " << ans << endl;
 
     return 0;
